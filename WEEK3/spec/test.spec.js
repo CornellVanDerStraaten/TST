e9118b571724a75912779837609f087565a8c614
Cornell van der Straaten <30183@ma-web.nl>
2020-09-22 15:52:27 +0200

    TST 22/09/2020

---
+++ WEEK3/spec/test.spec.js
@@ -0,0 +1,8 @@
+describe('Jasmine Testen', function () {
+    it('Tel 1 bij de voorraad op', function() {
+        console.log('Een opgeteld bij de voorraad');
+   });
+   it('Haal 1 van voorraad af', function() {
+       console.log('Een afgehaald van voorraad');
+   });
+});
